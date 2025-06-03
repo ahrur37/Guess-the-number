f0892eaa57a3096108889b1a5f546e94d8f2c370
212309 <212309@1220-001-22>
2025-06-03 17:55:53 +0300

    Добавьте файлы проекта.

---
+++ Guess the number/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Guess_the_number
+{
+    public partial class MainWindow : Window
+    {
+        public Random rand = new Random();
+        public int num;
+        public int count = 0;
+        public int n1 = 0;
+        public int n2 = 100;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            num = rand.Next(0, 5);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            num = rand.Next(0, 100);
+            count = 0;
+            Count.Text = $"{count}";
+            Hint.Text = "";
+        }
+
+        private void Check_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.Parse(Vvod.Text) == num)
+            {
+                MessageBox.Show($"Поздравляем, вы угадали число за {count} попыток!");
+                Hint.Text = "Угадал";
+            }
+            else
+            {
+                if (int.Parse(Vvod.Text) < num)
+                {
+                    Hint.Text = "Слишком мало";
+                }
+
+                if (int.Parse(Vvod.Text) > num)
+                {
+                    Hint.Text = "Слишком много";
+                }
+
+                if (int.Parse(Vvod.Text) + 5 >= num && int.Parse(Vvod.Text) - 5 <= num)
+                {
+                    MessageBox.Show("Вы близко");
+                }
+            }
+            count++;
+            Count.Text = $"{count}";
+            Histori.Items.Add($"{Vvod.Text} - {Hint.Text}");
+        }
+    }
+}
